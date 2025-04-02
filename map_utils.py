25f0c5c808b68f9f23239fdc65fa661bb90a3af0
uq88acaw <norul.gofran@fau.de>
2025-04-02 18:02:00 +0200

    Refactor code for improved readability and consistency in formatting

--- map_utils.py
+++ map_utils.py
@@ -4,8 +4,13 @@ import torchvision.transforms as transforms
 import numpy as np
 import PIL
 
-trans = transforms.Compose([transforms.ToTensor(),
-                            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
+trans = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    ]
+)
+
 
 def load_json(json_path):
     with open(json_path, "r") as f:
@@ -13,23 +18,26 @@ def load_json(json_path):
 
     return data
 
+
 def convert_input_images(img):
     img = img.resize((600, 300), resample=PIL.Image.NEAREST)
     img = trans(img)
 
     return img
 
+
 def convert_input_masks(mask):
     mask = mask.resize((600, 300), resample=PIL.Image.NEAREST)
-    mask = np.array(mask) 
+    mask = np.array(mask)
     mask = torch.from_numpy(mask)
     mask = mask.type(torch.float32)
 
     return mask
 
+
 def put_colors_in_grayscale_mask(grayscale_mask, n_classes, label_colors):
-    #convert grayscale to color
-    if not isinstance(grayscale_mask,np.ndarray):
+    # convert grayscale to color
+    if not isinstance(grayscale_mask, np.ndarray):
         temp = grayscale_mask.numpy()
     else:
         temp = grayscale_mask
@@ -47,5 +55,3 @@ def put_colors_in_grayscale_mask(grayscale_mask, n_classes, label_colors):
     rgb[:, :, 2] = b
 
     return rgb
-
-
--- onnx_export.py
+++ onnx_export.py
@@ -10,14 +10,15 @@ from city_training import cityscape_deeplab
 # We use 8 classes of cityscapes
 num_classes = 8
 
+
 def export_to_onnx(pytorch_model_path, onnx_model_path):
     # Load the PyTorch model
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     checkpoint = torch.load(pytorch_model_path, map_location=device)
 
     cityscape_model = cityscape_deeplab(num_classes)
     model = mapillary_deeplab(n_classes=num_classes, cityscape_model=cityscape_model)
-    
+
     model.load_state_dict(checkpoint["state_dict"])
 
     model.eval()
@@ -33,7 +34,6 @@ def export_to_onnx(pytorch_model_path, onnx_model_path):
         dummy_input,  # model input (or a tuple for multiple inputs)
         onnx_model_path,  # where to save the model (can be a file or file-like object)
         export_params=True,  # store the trained parameter weights inside the model file
-        
         do_constant_folding=True,  # whether to execute constant folding for optimization
         input_names=["input"],  # the model's input names
         output_names=["output"],  # the model's output names
